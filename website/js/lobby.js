6c0ab206cfec0e1e7ecbde04085ea48f83e1d3ef
Justin Bisignano <justinbis@fake.server>
2014-07-21 13:32:06 -0400

    Moving options pane
    Implemented the JS needed to move the options pane up and down

--- website/js/lobby.js
+++ website/js/lobby.js
@@ -2,12 +2,20 @@
  * When the options pane icon is clicked, roll the pane up or down
  */
 $('#optionsButton').click(function() {
-	// If the pane is hidden, show it by setting the down class
-
+	// If the pane is hidden
+	if ( $('#optionsPane').hasClass('optionsUp') )
+	{
+		// Show it by setting the down class
+		$('#optionsPane').removeClass('optionsUp').addClass('optionsDown');
 		// Set the icon to be the roll up icon
-
-
-	// If the pane is down, roll it up by setting the up class
-
+		$('#optionsPulldownIcon').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+	}
+	// Else the pane is visible
+	else
+	{
+		// Roll it up by setting the up class
+		$('#optionsPane').removeClass('optionsDown').addClass('optionsUp');
 		// Set the icon to be the roll down icon
+		$('#optionsPulldownIcon').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+	}
 });
