a49bdc53b6b74866db60f666ab971cd6f861bd76
Justin Bisignano <justinbis@fake.server>
2014-07-21 02:49:20 -0400

    Cleaned options pane, added pseudocode

---
+++ website/js/lobby.js
@@ -0,0 +1,13 @@
+/*
+ * When the options pane icon is clicked, roll the pane up or down
+ */
+$('#optionsButton').click(function() {
+	// If the pane is hidden, show it by setting the down class
+
+		// Set the icon to be the roll up icon
+
+
+	// If the pane is down, roll it up by setting the up class
+
+		// Set the icon to be the roll down icon
+});
